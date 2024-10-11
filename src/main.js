9e5dd9282e948409a34be4fb61680a5462cae87e
Hrebynakha Anatolii <hrebynakha@gmail.com>
2024-10-11 04:40:59 +0300

    add hw3

---
+++ src/main.js
@@ -0,0 +1,42 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { getImagesMarkup } from './js/render-functions';
+import { searchImages } from './js/pixabay-api';
+import { displayError } from './js/toast';
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+  searchForm: document.getElementById('searchPhotoForm'),
+  loader: document.querySelector('.loader'),
+};
+
+refs.searchForm.addEventListener('submit', handleSearchFormSubmit);
+
+function handleSearchFormSubmit(e) {
+  e.preventDefault();
+  const form = e.currentTarget;
+  const q = form.elements.q.value.trim();
+  refs.loader.classList.remove('loader-off');
+  searchImages(q)
+    .then(images => {
+      refs.gallery.innerHTML = '';
+      if (images.total === 0) {
+        refs.loader.classList.add('loader-off');
+        displayError();
+        return;
+      }
+      const imagesMarkup = getImagesMarkup(images);
+      refs.loader.classList.add('loader-off');
+      refs.gallery.insertAdjacentHTML('beforeend', imagesMarkup);
+      const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
+      lightbox.refresh();
+    })
+    .catch(e => {
+      console.error(e);
+      displayError();
+    })
+    .finally(() => form.reset());
+}
